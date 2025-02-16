0a676adb308b6ddaf5a795018a93c4b54a1fe2f8
Lehung59 <nguyenlehungsc@gmail.com>
2025-02-16 17:12:12 +0700

    init code

---
+++ src/main/java/com/example/rabiloo/RabilooApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabiloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabilooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabilooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabiloo/RabilooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabiloo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabilooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
